f9eef34ee389abeed97321861d4bbc1623959974
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2020-06-10 23:48:52 -0400

    Changed to take name to say 'Hello' to

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world!"
+	got := Hello("Junilu")
+	want := "Hello, Junilu!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
